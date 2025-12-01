c9acf6f1f0c8318731ade7ff9bae1a7efe64134c
sbaos <lop300520@gmail.com>
2025-12-02 01:50:13 +0700

    .

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World! Examate"', () => {
-      expect(appController.getHello()).toBe('Hello World! Examate');
+      expect(appController.getHello()).toBe('Hello Examate!');
     });
   });
 });
